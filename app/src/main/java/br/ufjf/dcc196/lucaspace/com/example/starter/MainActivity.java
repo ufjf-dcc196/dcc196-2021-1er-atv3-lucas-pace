978971d69ffa2d7ad967f0ad6cd8ceea4d186b2a
Lucas Pace <lucas.pace@hotmail.com.br>
2021-06-26 02:57:13 -0300

    criando botao de soma e a funcao para executar a operacao

--- app/src/main/java/br/ufjf/dcc196/lucaspace/com/example/starter/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/lucaspace/com/example/starter/MainActivity.java
@@ -3,7 +3,11 @@ package br.ufjf.dcc196.lucaspace.com.example.starter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
 
         //TextView saida = findViewById(R.id.labelinput1);
         //saida.setText("Bem vindo");
+    }
+
+
+    public void sum(View view){
+
+        EditText input1 = findViewById(R.id.input1);
+        EditText input2 = findViewById(R.id.input2);
+
+
+        Double input1D = Double.parseDouble(input1.getText().toString());
+        Double input2D = Double.parseDouble(input2.getText().toString());
+        Double resultSum = input1D + input2D;
+
+        TextView result  = findViewById(R.id.result);
+        result.setText(resultSum.toString());
     }
 }
